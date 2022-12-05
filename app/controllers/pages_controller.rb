ad209c0eefff33bef8ccc61f3018c08650a36149
caroline bras <brascaroline64@gmail.com>
2022-12-05 08:21:12 +0100

    Did some front end : Navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
   end
 
   def about
+    @categories = ["Dons", "Services", "Covoiturage", "Professionels", "Offres d'emplois"]
+
+    if params[:categorie]
+      @categories = @categories.select { |categorie| categorie.start_with?(params[:categorie]) }
+    end
+    raise
   end
 
   def contact
